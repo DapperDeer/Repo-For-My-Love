db9d15349b752d5866abd14a94892e36d89f8f8d
DapperDeer <harin414@gmail.com>
2020-08-01 00:29:45 -0700

    new rng for inclusion

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
 function GenerateRandomPetie() {
-	var rng = Math.floor((Math.random() * 10) + 2);
+	var rng = getRandomIntInclusive(1, 11);
 	var path = document.getElementById("petie");
 	path.src = "../images/dog" + rng + ".jpg";	
 }
